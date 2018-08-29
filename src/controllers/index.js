92a8b883ea39fc707f30332a583e3dcf774755ac
donia98 <nadooshah98@gmail.com>
2018-08-29 14:42:43 +0300

    server and error page

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,1 +1,15 @@
 // routes
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+// import home route controller
+const home = require('./home');
+const error = require('./error');
+
+// add home route
+router.get('/', home.get);
+router.use(error.client);
+router.use(error.server);
+
+module.exports = router;
