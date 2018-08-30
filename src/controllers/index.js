d7f58520ae68ebf65170e38d102a4b29372f3d75
MarwanElkhoudary <marwangaza@hotmail.com>
2018-08-30 09:39:37 +0300

    add portfolio

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,1 +1,16 @@
 // routes
+const express = require('express');
+const path = require('path');
+const router = express.Router();
+
+// import home route controller
+const home = require('./home');
+const error = require('./error');
+const portFolio = require('./portFolio');
+
+router.get('/', home.get);
+router.get('/portFolio/:id', portFolio.get);
+// router.use(error.client);
+// router.use(error.server);
+
+module.exports = router;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
 // server
+const app = require('./app');
+
+app.listen(app.get('port'), () => {
+  console.log('App running on port', app.get('port'));
+});
